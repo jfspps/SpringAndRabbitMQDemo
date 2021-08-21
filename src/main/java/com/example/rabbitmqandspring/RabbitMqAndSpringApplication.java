46ca493f64c7145009f7779e6c04fd2492de1255
jfspps <jfsppsDev@gmail.com>
2021-08-21 21:19:05 +0100

    first commit

---
+++ src/main/java/com/example/rabbitmqandspring/RabbitMqAndSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqandspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqAndSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMqAndSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rabbitmqandspring/RabbitMqAndSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmqandspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqAndSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
